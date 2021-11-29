1d54ebc6fac60692e99f1dff96bb1395181f6469
Mike Knoles <MKnoles78@gmail.com>
2021-11-28 20:24:54 -0500

    added variables and for loop

--- src/Largest.java
+++ src/Largest.java
@@ -1,9 +1,23 @@
+
 //Mike Knoles 3.11 Midterm
 //Largest.java
+import java.util.Scanner;
+
 public class Largest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		// create an array to store 10 integers
+		int numbers[] = new int[10];
+		int number = 0; // integer most recently input by user
+		int largest = 0; // largest number found so far
+
+		// prompt user for 10 integer values
+		System.out.println("Please enter 10 integers: ");
+		for (number = 0; number < numbers.length; number++) {
+			numbers[number] = input.nextInt();
+		}
 
 	}
 
